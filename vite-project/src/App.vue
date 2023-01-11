e6248383da8aa7926a8a2855e499d4f64e71b1a0
leo5295 <leonardo.martellosio@gmail.com>
2023-01-11 20:14:19 +0100

    first commit

---
+++ vite-project/src/App.vue
@@ -0,0 +1,31 @@
+<script>
+
+import AppHeader from './components/AppHeader.vue';
+import AppFooter from './components/AppFooter.vue';
+
+
+export default {
+  components: {
+    AppHeader,
+    AppFooter
+  }
+}
+</script>
+
+<template>
+  <div>
+  <AppHeader />
+  <AppFooter />
+  </div>
+</template>
+
+
+<style lang="scss">
+@use './styles/generals.scss' as *;
+
+@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+
+*{
+  font-family: 'Roboto', sans-serif;
+}
+</style>
---
+++ vite-project/src/components/AppHeader.vue
@@ -0,0 +1,120 @@
+<script>
+export default {
+  data() {
+      return {
+          menu: [
+              {
+                  label: 'CHARACTERS',
+                  url: '#',
+                  active: false
+              },
+              {
+                  label: 'COMICS',
+                  url: '#',
+                  active: true
+              },
+              {
+                  label: 'MOVIES',
+                  url: '#',
+                  active: false
+              },
+              {
+                  label: 'TV',
+                  url: '#',
+                  active: false
+              },
+              {
+                  label: 'GAMES',
+                  url: '#',
+                  active: false
+              },
+              {
+                  label: 'COLLECTIBLES',
+                  url: '#',
+                  active: false
+              },
+              {
+                  label: 'VIDEOS',
+                  url: '#',
+                  active: false
+              },
+              {
+                  label: 'FANS',
+                  url: '#',
+                  active: false
+              },
+              {
+                  label: 'NEWS',
+                  url: '#',
+                  active: false
+              },
+              {
+                  label: 'SHOP',
+                  url: '#',
+                  active: false
+              },
+          ]
+      }
+  }
+}
+</script>
+
+
+<template lang="">
+  <header>
+     <a href="/" class="brand">
+        <img class="logo" src="../assets/img/dc-logo.png" alt="">
+     </a>
+     <nav>
+      <ul>
+          <li v-for="(item, index) in menu" :key="index" class="link" :class="item.active ? 'active' : ''">
+              <a :href="item.url" class="link">
+              {{item.label}}
+              </a>
+          </li>
+      </ul>
+     </nav>
+  </header>
+</template>
+
+<style scoped lang="scss">
+header{
+    height: 100px;
+    display: flex;
+    align-items: center;
+    background-color: #fff;
+}
+
+header > a{
+    width: 30%;
+    text-align: center;
+}
+
+nav{
+    width: 70%;
+    text-align: center;
+    font-size: 15px;
+
+}
+.logo{
+    height: 70px;
+}
+
+ul{
+    list-style: none;
+}
+
+li{
+    display: inline;
+    a:link { text-decoration: none; }
+    padding: 15px;
+    margin: 2px;
+    border-bottom: 5px solid transparent;
+
+}
+
+li.active{
+        border-block-color: blue;
+}
+
+</style>
